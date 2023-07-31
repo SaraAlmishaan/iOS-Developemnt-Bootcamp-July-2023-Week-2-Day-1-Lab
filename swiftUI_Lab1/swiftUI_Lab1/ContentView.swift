b5029343389891f4a3d99198b222dcd6798d1c91
SaraAlmishaan <140312268+SaraAlmishaan@users.noreply.github.com>
2023-07-31 08:09:26 +0300

    Add files via upload

---
+++ swiftUI_Lab1/swiftUI_Lab1/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  swiftUI_Lab1
+//
+//  Created by Sara on 31/07/2023.
+/*
+ 
+
+
+ 
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      
+ ZStack{
+     Color.black.ignoresSafeArea()
+     Image("backgroundpic").resizable()
+     VStack{
+           
+         Image("profile").resizable().frame(width: 200, height:200, alignment: .top).padding()
+         
+         HStack{
+             Text("Sara Fahad Mishaan").bold().padding(.leading, 80)
+             Button(action: { print("Hi")} , label: { Text("view profile")})
+             }
+           
+         Text("A fresh graduated with a Bachelor's degree in Computer Science from Al-Imam University")
+               .font(.subheadline)
+               .fontWeight(.light)
+               .foregroundColor(Color.black)
+               .multilineTextAlignment(.center)
+               .frame(width: 360, height: 40, alignment: .top)
+               .padding()
+     }
+        
+  }
+     
+        
+        
+        
+ }
+}
+                  
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI_Lab1/swiftUI_Lab1/swiftUI_Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_Lab1App.swift
+//  swiftUI_Lab1
+//
+//  Created by Sara on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
